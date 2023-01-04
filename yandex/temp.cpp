c4011133e631e84b3ce13f85eb758119c6e9e0ce
rashid <rassant@mail.ru>
2023-01-04 15:52:34 +0300

    начало работы над поиском в яндексе

--- yandex/temp.cpp
+++ yandex/temp.cpp
@@ -1,78 +1,146 @@
+#include <algorithm>
 #include <iostream>
 #include <set>
 #include <string>
+#include <utility>
 #include <vector>
 
 using namespace std;
+                                                 // чтение строки 
+string 
+ReadLine() {
 
+        string s;
 
-vector<string>  
-SplitIntoWords  ( const string&  text ) 
-{
-            vector<string> words;
-                       string  word;
-
-
-                for  ( const char c : text ) {
-                    if  ( c == ' ' ) {
-                        if  ( !word.empty( )) {
-                            words.push_back ( word );
-                            word.clear      ( );
-                        }
-                    } else {
-                        word += c;
-                    }
-                }
-
-
-                if  ( !word.empty( )) {
-                      words.push_back ( word );
-                }
-                return  words;
+        getline(cin, s);
+        return s;
 }
 
+int 
+ReadLineWithNumber() {
 
+        int result = 0;
 
-set<string>  
-ParseStopWords  ( const string&  text ) {
-    set<string>  stop_words;
-    for (const string&  word : SplitIntoWords ( text )) {
-        stop_words.insert( word );
-    }
-    return  stop_words;
+        cin >> result;
+        ReadLine();
+        return result;
 }
 
+vector<string> 
+SplitIntoWords  ( const string&  text ) {
 
+        vector<string>  words;
+               string   word;
 
-vector<string>  
-ParseQuery  ( const string      & text,
-              const set<string> & stop_words) {
-
-    vector<string>  words;
-
-    for  ( const string&  word : SplitIntoWords ( text )) {
-        if ( stop_words.count( word ) == 0 ) {
+        for  ( const char c : text ) {
+            if  ( c  ==  ' ' ) {
+                if  ( !word.empty( ) ) {
+                    words.push_back( word );
+                     word.clear( );
+                }
+            } else {
+                word  +=  c;
+            }
+        }
+        if  ( !word.empty() ) {
             words.push_back( word );
         }
-    }
+
     return  words;
 }
 
+set<string> 
+ParseStopWords  ( const string& text ) {
 
+        set<string>  stop_words;
 
-int main() {
-    // Read stop words
+        for  ( const string& word : SplitIntoWords( text )) {
+            stop_words.insert( word );
+        }
+        return  stop_words;
+}
 
-    /*------------------->*/ string /*--->*/stop_words_joined;
-    /*------------------->*/ getline ( cin, stop_words_joined);
-    set<string> stop_words = ParseStopWords(stop_words_joined);
-	
-    // Read query
-    /*------------------------>*/ string  /*--->*/ query;
-    /*------------------------>*/ getline   ( cin, query );
-    vector<string> query_words = ParseQuery      ( query, stop_words );
-	
-    for (const string& word : query_words) {
-        cout << '[' << word << ']' << endl;
-    }
+vector<string> 
+SplitIntoWordsNoStop  ( const     string&  text, 
+                        const set<string>& stop_words ) 
+{
+        vector<string>  words;
+
+                                                 // проходим по всем словам из текста и проверяем,
+                                                 // что их нет в списке стоп-слов
+        for  ( const string  &word : SplitIntoWords( text )) {
+            if  ( stop_words.count( word )  ==  0) {
+                words.push_back( word );
+            }
+        }
+                                                 // вернём результат без стоп-слов
+        return words;
+}
+
+void 
+AddDocument  ( vector<vector<string>>  &documents,
+                   const set<string>   &stop_words,
+                   const     string    &document )
+{
+        const vector<string>  words  =  SplitIntoWordsNoStop  ( document,  stop_words );
+        documents.push_back( words );
+}
+
+                                                 // Разбирает text на слова и возвращает только 
+                                                 // те из них, которые не входят в stop_words
+set<string> 
+ParseQuery  ( const     string  &text, 
+              const set<string> &stop_words) 
+{
+        set<string>  query_words;
+
+                                                 // Напишите код функции
+
+        return  query_words;
+}
+
+                                                 // Возвращает true, если среди слов документа 
+                                                 // (document_words) встречаются слова 
+                                                 // поискового запроса query_words
+bool 
+MatchDocument  ( const vector<string>  &document_words,  
+                 const    set<string>  &query_words) {
+        // Напишите код функции
+
+        return false;
+}
+
+                                                 // Возвращает массив id документов, подходящих 
+                                                 // под запрос query Стоп-слова исключаются из поиска
+vector<int> 
+FindDocuments  ( const vector<vector<string>>  &documents, 
+                 const    set<string>  &stop_words,
+                 const        string   &query) 
+{
+        vector<int> matched_documents;
+
+        // Напишите код функции
+        // Воспользуйте вспомогательными функциями ParseQuery, MatchDocument
+        // В качестве id документа используйте его индекс в массиве documents
+
+        return matched_documents;
+}
+
+int 
+main() {
+        const     string  stop_words_joined  =  ReadLine( );
+        const set<string> stop_words         =  ParseStopWords( stop_words_joined );
+                                                 // Read documents
+        vector<vector<string>>  documents;
+        const int  document_count  =  ReadLineWithNumber( );
+
+        for  ( int document_id = 0; document_id < document_count; ++document_id ) {
+                AddDocument  ( documents,  stop_words,  ReadLine( ) );
+        }
+
+        const string query  =  ReadLine( );
+
+        for  ( const int document_id : FindDocuments( documents, stop_words, query )) {
+                cout<<  "{ document_id = "s  <<  document_id  <<  " }"s  <<endl;
+        }
 }
